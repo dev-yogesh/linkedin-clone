5575a4b80489c8c34f2d13e52dc9cdba1d9cf4e5
dev-yogesh <yogeshgupta279@gmail.com>
2022-03-26 19:14:37 +0530

    wip: home header ui creation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
+
+const MainHome = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='' element={<Login />} />
+        <Route path='/home' element={<MainHome />} />
       </Routes>
     </Router>
   );
